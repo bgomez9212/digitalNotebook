b02dcc721cca67163c350e40347d8bdfe9bcc957
Brandon Gomez <bgomez9212@gmail.com>
2024-02-02 14:21:38 -0800

    metro config and basic layout of login page

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,9 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TextInput } from "react-native";
 
 export default function Home() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Hello World!</Text>
+    <View className="flex-1 items-center justify-center bg-black">
+      <Image
+        source={require("../assets/Notebook.png")}
+        resizeMode="contain"
+        className="w-full mb-10"
+      />
+      <TextInput className="w-1/2 bg-white h-5 p-4" />
+      <TextInput className="w-1/2 bg-white h-5 p-4 mt-2" />
     </View>
   );
 }
--- client/firebase.ts
+++ client/firebase.ts
@@ -8,13 +8,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAfGySeuAbrYDoVIbgKLHhMK_H7oSXV-LQ",
-  authDomain: "thenotebook-53588.firebaseapp.com",
-  projectId: "thenotebook-53588",
-  storageBucket: "thenotebook-53588.appspot.com",
-  messagingSenderId: "760490551001",
-  appId: "1:760490551001:web:1f586349ba6f53b67b40c8",
-  measurementId: "G-825SMLTZG3",
+  apiKey: "AIzaSyBpp3HRxp3TRq26U84aW0ZKmvZEiVwp7V0",
+  authDomain: "thenotebook-e6229.firebaseapp.com",
+  projectId: "thenotebook-e6229",
+  storageBucket: "thenotebook-e6229.appspot.com",
+  messagingSenderId: "55908572804",
+  appId: "1:55908572804:web:cff1989ee5be3e2011acd1",
 };
 
 // Initialize Firebase

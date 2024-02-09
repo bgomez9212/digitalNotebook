5f81113e5b3308221ceb5e4e6eae09ba13495ad6
Brandon Gomez <bgomez9212@gmail.com>
2024-02-09 11:52:14 -0800

    navigation with icons

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,9 +1,41 @@
 import { Tabs } from "expo-router";
-
+import { Entypo } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 export default function Layout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="tabHome" />
+    <Tabs
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#477CB9",
+        },
+        headerTitleStyle: {
+          color: "white",
+        },
+        tabBarActiveTintColor: "#477CB9",
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tabs.Screen
+        name="Home"
+        options={{
+          tabBarIcon: () => <Entypo name="home" size={24} color="black" />,
+        }}
+      />
+      <Tabs.Screen
+        name="Search"
+        options={{
+          tabBarIcon: () => (
+            <FontAwesome name="search" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        options={{
+          tabBarIcon: () => <Ionicons name="person" size={24} color="black" />,
+        }}
+      />
     </Tabs>
   );
 }

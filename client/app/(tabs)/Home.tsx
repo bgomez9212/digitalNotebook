5a2731beb7dd77ab3987b943c42475cb63fbd0b3
Brandon Gomez <bgomez9212@gmail.com>
2024-02-29 14:40:04 -0800

    page layout changes

--- client/app/(tabs)/Home.tsx
+++ client/app/(tabs)/Home.tsx
@@ -1,5 +1,10 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import tw from "../../tailwind";
 
 export default function Home() {
-  return <Text>Home</Text>;
+  return (
+    <View style={tw`bg-black h-full flex justify-center items-center`}>
+      <Text style={tw`text-white`}>Home</Text>
+    </View>
+  );
 }
--- client/app/(tabs)/Search.tsx
+++ client/app/(tabs)/Search.tsx
@@ -1,5 +1,10 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import tw from "../../tailwind";
 
 export default function Profile() {
-  return <Text>Search</Text>;
+  return (
+    <View style={tw`bg-black h-full flex justify-center items-center`}>
+      <Text style={tw`text-white`}>Search</Text>
+    </View>
+  );
 }

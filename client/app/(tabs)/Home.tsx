082e7960642c591b6b258eed7f41114d7c5ed6fc
Brandon Gomez <bgomez9212@gmail.com>
2024-02-29 16:49:07 -0800

    frontend receiving response from server

--- client/app/(tabs)/Home.tsx
+++ client/app/(tabs)/Home.tsx
@@ -1,25 +1,38 @@
 import { Text, View } from "react-native";
 import tw from "../../tailwind";
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 export default function Home() {
   const { isPending, error, data } = useQuery({
     queryKey: ["repoData"],
     queryFn: () =>
-      fetch("https://api.github.com/repos/TanStack/query").then((res) =>
-        res.json()
-      ),
+      axios.get("http://localhost:3000/api").then((res) => res.data),
   });
 
   console.log(data);
 
   // if (isPending) return 'Loading...'
+  if (isPending) {
+    return (
+      <View style={tw`bg-black h-full flex justify-center items-center`}>
+        <Text style={tw`text-white`}>Loading...</Text>
+      </View>
+    );
+  }
 
   // if (error) return 'An error has occurred: ' + error.message
+  if (error) {
+    return (
+      <View style={tw`bg-black h-full flex justify-center items-center`}>
+        <Text style={tw`text-white`}>{error.message}</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={tw`bg-black h-full flex justify-center items-center`}>
-      <Text style={tw`text-white`}>Home</Text>
+      <Text style={tw`text-white`}>{data}</Text>
     </View>
   );
 }

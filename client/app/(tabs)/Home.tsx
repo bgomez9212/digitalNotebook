412bed4aa1fcacea63c68addc2b1eaed77b0d0a4
Brandon Gomez <bgomez9212@gmail.com>
2024-03-01 16:10:34 -0800

    table created

--- client/app/(tabs)/Home.tsx
+++ client/app/(tabs)/Home.tsx
@@ -1,42 +1,13 @@
 import { Text, View } from "react-native";
 import tw from "../../tailwind";
+import RecentMatchTable from "../../components/RecentMatchTable";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export default function Home() {
-  const {
-    isPending,
-    error,
-    data: events,
-  } = useQuery({
-    queryKey: ["events"],
-    queryFn: () =>
-      axios.get("http://localhost:3000/api/events").then((res) => res.data),
-  });
-
-  console.log(events);
-
-  // if (isPending) return 'Loading...'
-  if (isPending) {
-    return (
-      <View style={tw`bg-black h-full flex justify-center items-center`}>
-        <Text style={tw`text-white`}>Loading...</Text>
-      </View>
-    );
-  }
-
-  // if (error) return 'An error has occurred: ' + error.message
-  if (error) {
-    return (
-      <View style={tw`bg-black h-full flex justify-center items-center`}>
-        <Text style={tw`text-white`}>{error.message}</Text>
-      </View>
-    );
-  }
-
   return (
     <View style={tw`bg-black h-full flex justify-center items-center`}>
-      <Text style={tw`text-white`}>Hello World</Text>
+      <RecentMatchTable />
     </View>
   );
 }
--- client/components/RecentMatchTable.tsx
+++ client/components/RecentMatchTable.tsx
@@ -1,1 +1,63 @@
-export default function RecentMatchTable() {}
+import { View, Text, StyleSheet } from "react-native";
+import { DataTable } from "react-native-paper";
+import tw from "../tailwind";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export default function RecentMatchTable() {
+  const {
+    isPending,
+    error,
+    data: events,
+  } = useQuery({
+    queryKey: ["events"],
+    queryFn: () =>
+      axios.get("http://localhost:3000/api/events").then((res) => res.data),
+  });
+
+  // if (isPending) return 'Loading...'
+  if (isPending) {
+    return (
+      <View style={tw`bg-black h-full flex justify-center items-center`}>
+        <Text style={tw`text-white`}>Loading...</Text>
+      </View>
+    );
+  }
+
+  // if (error) return 'An error has occurred: ' + error.message
+  if (error) {
+    return (
+      <View style={tw`bg-black h-full flex justify-center items-center`}>
+        <Text style={tw`text-white`}>{error.message}</Text>
+      </View>
+    );
+  }
+  return (
+    <DataTable style={tw`bg-white`}>
+      <DataTable.Header>
+        <View style={tw`flex-1 items-center justify-center py-3`}>
+          <Text>Promotion</Text>
+        </View>
+        <View style={tw`flex-2 items-center justify-center py-3 `}>
+          <Text>Event</Text>
+        </View>
+        <View style={tw`flex-1 items-center justify-center py-3`}>
+          <Text>Date</Text>
+        </View>
+      </DataTable.Header>
+      {events.map((event) => (
+        <DataTable.Row key={event.id}>
+          <View style={tw`flex-1 justify-center items-center`}>
+            <Text>{event.promotion_id}</Text>
+          </View>
+          <View style={tw`flex-2 justify-center items-center`}>
+            <Text>{event.title}</Text>
+          </View>
+          <View style={tw`flex-1 justify-center items-center`}>
+            <Text>{event.date}</Text>
+          </View>
+        </DataTable.Row>
+      ))}
+    </DataTable>
+  );
+}

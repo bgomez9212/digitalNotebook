e87a8f22cf2672150c46560691986133d7ce6fb3
Brandon Gomez <bgomez9212@gmail.com>
2024-03-04 18:26:31 -0800

    server sends top 5 rated matches of the month

--- server/model.js
+++ server/model.js
@@ -15,51 +15,56 @@ module.exports = {
     return "Hello World";
   },
   getTopRatedMatches: async () => {
+    const date = new Date();
+
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+    let lastMonth = `${year}-${month}-${day}`;
+
     const matchesArr = [];
     const { rows: results } = await pool.query(
       `
-        SELECT participants.match_id, participants.team, wrestlers.name, matches.rating FROM participants
-        JOIN wrestlers ON participants.wrestler_id = wrestlers.id
-        JOIN matches ON participants.match_id = matches.id
-        WHERE match_id IN (
-          SELECT matches.id FROM matches
-          JOIN events ON matches.event_id = events.id
-          WHERE events.date > '2024-02-22'::DATE
-          ORDER BY rating DESC
-          LIMIT 5
-        );
-      `
+      SELECT participants.match_id, participants.team, wrestlers.name, matches.rating, matches.event_id, events.date FROM participants
+      JOIN wrestlers ON participants.wrestler_id = wrestlers.id
+      JOIN matches ON participants.match_id = matches.id
+      JOIN events ON events.id = matches.event_id
+      WHERE match_id IN (
+        SELECT matches.id FROM matches
+        JOIN events ON matches.event_id = events.id
+        WHERE events.date > $1::DATE
+        ORDER BY rating DESC, events.date DESC
+        LIMIT 5
+      )
+      ORDER BY date DESC;
+      `,
+      [lastMonth]
     );
-    for (let matchData of results) {
+    let matchObj = {
+      event_id: results[0].event_id,
+      match_id: results[0].match_id,
+      rating: results[0].rating,
+      wrestlers: [[results[0].name]],
+    };
+    // skip first entry since that is manually inserted
+    for (let matchData of results.slice(1, results.length)) {
+      if (matchObj.match_id === matchData.match_id) {
+        if (matchObj.wrestlers[matchData.team]) {
+          matchObj.wrestlers[matchData.team].push(matchData.name);
+        } else {
+          matchObj.wrestlers[matchData.team] = [matchData.name];
+        }
+      } else {
+        matchesArr.push({ ...matchObj });
+        matchObj.event_id = matchData.event_id;
+        matchObj.match_id = matchData.match_id;
+        matchObj.rating = matchData.rating;
+        matchObj.wrestlers = [[matchData.name]];
+      }
+      if (results.indexOf(matchData) === results.length - 1) {
+        matchesArr.push({ ...matchObj });
+      }
     }
+    return matchesArr;
   },
 };
-
-// [
-//   {
-//     match_id: 10,
-//     rating: 5,
-//     wrestlers:
-//     [
-//       ["Chris Jericho"],
-//       ["Atlantis Jr"]
-//     ]
-//   },
-//   {
-//     match_id: 5,
-//     rating: 5,
-//     wrestlers:
-//     [
-//       [
-//         "Anthony Bowens",
-//         "Austin Gunn",
-//         "Max Caster"
-//       ],
-//       [
-//         "Alex Reynolds",
-//         "Evil Uno",
-//         "John Silver"
-//       ]
-//     ]
-//   }
-// ]

445482e98f1a04ff26c405e168f6498fab17390d
Brandon Gomez <bgomez9212@gmail.com>
2024-03-15 18:29:46 -0700

    fix top rated table

--- client/components/TopRatedMatchesTable.tsx
+++ client/components/TopRatedMatchesTable.tsx
@@ -37,12 +37,6 @@ export default function TopRatedMatchesTable() {
   }
 
   return (
-    <View>
-      <Text>Placeholder</Text>
-    </View>
-  );
-
-  return (
     <DataTable style={tw`bg-white mt-16`}>
       <DataTable.Header>
         <View style={tw`flex-1 items-center justify-center py-3`}>
@@ -54,18 +48,27 @@ export default function TopRatedMatchesTable() {
           key={match.match_id}
           onPress={() => router.push(`/(tabs)/Home/${match.event_id}`)}
         >
-          <DataTable.Row style={tw`h-fit p-2`}>
-            <View style={tw`flex-1 justify-center items-center`}>
-              <Image
-                style={tw`w-24 h-12`}
-                source={require("../assets/aew-logo.jpg")}
-              />
-            </View>
-            <View style={tw`flex-2 p-2 justify-center`}>
-              <Text>{match.wrestlers}</Text>
-            </View>
-            <View style={tw`flex-1 justify-center items-center`}>
-              <Text>{match.rating}</Text>
+          <DataTable.Row>
+            <View style={tw`w-full items-center py-2`}>
+              {match.championships && (
+                <View>
+                  <Text>{match.championships}</Text>
+                </View>
+              )}
+              <View style={tw`flex flex-row py-2 items-center`}>
+                <View style={tw`flex-1`}>
+                  <Image
+                    style={tw`w-24 h-12`}
+                    source={require("../assets/aew-logo.jpg")}
+                  />
+                </View>
+                <View style={tw`flex-2.5`}>
+                  <Text>{match.participants}</Text>
+                </View>
+              </View>
+              <View style={tw`w-full`}>
+                <Text style={tw`text-right pr-3`}>{match.rating}</Text>
+              </View>
             </View>
           </DataTable.Row>
         </TouchableOpacity>

0ac57f4fb37a36b0860b597cd8b74d56b247b9af
Brandon Gomez <bgomez9212@gmail.com>
2024-03-17 17:03:12 -0700

    add picker and format rating modal

--- client/app/(tabs)/Home/RatingModal.tsx
+++ client/app/(tabs)/Home/RatingModal.tsx
@@ -1,10 +1,13 @@
-import { View, Text, ActivityIndicator } from "react-native";
+import { View, Text, ActivityIndicator, Pressable } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 import { useLocalSearchParams } from "expo-router";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import tw from "../../../tailwind";
+import { useState } from "react";
 
 export default function RatingModal() {
+  const [rating, setRating] = useState(2);
   const { id } = useLocalSearchParams();
   const {
     isPending,
@@ -50,6 +53,45 @@ export default function RatingModal() {
             : "submit the first rating"}
         </Text>
       </View>
+      <Picker
+        style={tw`w-1/2`}
+        selectedValue={rating}
+        onValueChange={(itemValue, itemIndex) => setRating(itemValue)}
+      >
+        <Picker.Item label="¼" value={0.25} color="white" />
+        <Picker.Item label="½" value={0.5} color="white" />
+        <Picker.Item label="¾" value={0.75} color="white" />
+        <Picker.Item label="1" value={1} color="white" />
+        <Picker.Item label="1 ¼" value={1.25} color="white" />
+        <Picker.Item label="1 ½" value={1.5} color="white" />
+        <Picker.Item label="1 ¾" value={1.75} color="white" />
+        <Picker.Item label="2" value={2} color="white" />
+        <Picker.Item label="2 ¼" value={2.25} color="white" />
+        <Picker.Item label="2 ½" value={2.5} color="white" />
+        <Picker.Item label="2 ¾" value={2.75} color="white" />
+        <Picker.Item label="3" value={3} color="white" />
+        <Picker.Item label="3 ¼" value={3.25} color="white" />
+        <Picker.Item label="3 ½" value={3.5} color="white" />
+        <Picker.Item label="3 ¾" value={3.75} color="white" />
+        <Picker.Item label="4" value={4} color="white" />
+        <Picker.Item label="4 ¼" value={4.25} color="white" />
+        <Picker.Item label="4 ½" value={4.5} color="white" />
+        <Picker.Item label="4 ¾" value={4.75} color="white" />
+        <Picker.Item label="5" value={5} color="white" />
+      </Picker>
+      <View style={tw`py-4`}>
+        {rating > 1 ? (
+          <Text style={tw`text-white`}>Rate this match {rating} stars</Text>
+        ) : (
+          <Text style={tw`text-white`}>Rate this match {rating} star</Text>
+        )}
+      </View>
+      <Pressable
+        onPress={() => console.log("rated", rating)}
+        style={tw`bg-blue w-1/4 p-4 items-center justify-center rounded-md`}
+      >
+        <Text style={tw`text-white text-lg`}>Submit</Text>
+      </Pressable>
     </View>
   );
 }

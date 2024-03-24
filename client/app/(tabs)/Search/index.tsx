b9ef1f89dab6d9c97b44308b4537fab7e835a3df
Brandon Gomez <bgomez9212@gmail.com>
2024-03-24 13:03:36 -0700

    set searchbar at top

--- client/app/(tabs)/Search/index.tsx
+++ client/app/(tabs)/Search/index.tsx
@@ -6,9 +6,7 @@ export default function Profile() {
   const [userSearch, setUserSearch] = useState("");
   console.log(userSearch);
   return (
-    <View
-      style={tw`flex-1 bg-darkGrey w-full justify-center items-center border`}
-    >
+    <View style={tw`flex-1 bg-darkGrey w-full pt-12 items-center border`}>
       <TextInput
         style={tw`bg-white w-99 h-10 mb-2 px-2 pb-2 rounded-md text-base`}
         value={userSearch}

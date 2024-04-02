2e2b1fb144b8ddb186990fe9fa2baf4d6d44dbbf
Brandon Gomez <bgomez9212@gmail.com>
2024-04-02 16:31:58 -0700

    minor security changes to server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 const cors = require("cors");
 const router = require("./router.js");
+const dotenv = require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(process.env.SERVERPORT, () => {
+  console.log(`Server running`);
 });

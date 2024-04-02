61876239928dd1b86b5f608e59cea38cd8424203
Brandon Gomez <bgomez9212@gmail.com>
2024-04-02 13:54:23 -0700

    reload home page on scroll down

--- client/app/(tabs)/Home/index.tsx
+++ client/app/(tabs)/Home/index.tsx
@@ -1,11 +1,19 @@
-import { ScrollView, View } from "react-native";
+import { RefreshControl, ScrollView, View } from "react-native";
 import tw from "../../../tailwind";
 import RecentMatchTable from "../../../components/RecentEventsTable";
 import TopRatedMatchesTable from "../../../components/TopRatedMatchesTable";
+import { useCallback, useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function Home() {
+  const [refreshing, setRefreshing] = useState(false);
+  const queryClient = useQueryClient();
+  const onRefresh = useCallback(() => {
+    queryClient.invalidateQueries();
+  }, []);
   return (
     <ScrollView>
+      <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
       <View style={tw`bg-darkGrey flex items-center min-h-200`}>
         <RecentMatchTable />
         <TopRatedMatchesTable />

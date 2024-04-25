204aa6997121caeb0e53f7b44320dbafa0a77f42
Brandon Gomez <bgomez9212@gmail.com>
2024-04-25 14:40:05 -0700

    add pie chart to app

--- client/app/(tabs)/Profile.tsx
+++ client/app/(tabs)/Profile.tsx
@@ -9,6 +9,10 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import MatchRow from "../../components/MatchRow";
 import { ActivityIndicator } from "react-native-paper";
+import { PieChart } from "react-native-chart-kit";
+import { Dimensions } from "react-native";
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
 // TODO Put Signout button here
 export default function Profile() {
   const user = useContext(AuthContext);
@@ -46,34 +50,100 @@ export default function Profile() {
     return <Redirect href="../../" />;
   }
 
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false, // optional
+  };
+
+  const data = [
+    {
+      name: "Seoul",
+      population: 21500000,
+      color: "rgba(131, 167, 234, 1)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "Toronto",
+      population: 2800000,
+      color: "#F00",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "Beijing",
+      population: 527612,
+      color: "red",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "New York",
+      population: 8538000,
+      color: "#ffffff",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "Moscow",
+      population: 11920000,
+      color: "rgb(0, 0, 255)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+  ];
+
   return (
-    <View style={tw`flex-1 bg-darkGrey items-center`}>
-      <Pressable
-        style={tw`h-15 w-30 bg-blue flex justify-center items-center rounded-xl my-10`}
-        onPress={displayAlert}
-      >
-        <Text style={tw`text-white text-lg`}>Sign Out</Text>
-      </Pressable>
-      <Text style={tw`text-xl text-white underline mb-10`}>
-        Matches You Have Rated
-      </Text>
-      <ScrollView showsVerticalScrollIndicator={false} style={tw`w-9/10`}>
-        {isError ? (
-          <Text>There seems to be an error..</Text>
-        ) : isLoading ? (
-          <ActivityIndicator />
-        ) : (
-          userRatings.map((match, i) => (
-            <MatchRow
-              key={match.match_id}
-              match={match}
-              eventTitle={match.event_title}
-              display="Else"
-              hideBottomBorder={i === userRatings.length - 1}
-            />
-          ))
-        )}
-      </ScrollView>
-    </View>
+    <ScrollView nestedScrollEnabled={true}>
+      <View style={tw`flex-1 bg-darkGrey items-center`}>
+        <Pressable
+          style={tw`h-15 w-30 bg-blue flex justify-center items-center rounded-xl my-5`}
+          onPress={displayAlert}
+        >
+          <Text style={tw`text-white text-lg`}>Sign Out</Text>
+        </Pressable>
+        <PieChart
+          style={tw`mb-10 flex justify-center items-center`}
+          data={data}
+          width={screenWidth}
+          height={screenWidth}
+          chartConfig={chartConfig}
+          accessor={"population"}
+          backgroundColor={"transparent"}
+          paddingLeft={"0"}
+          hasLegend={false}
+          center={[screenWidth - 300, 0]}
+        />
+        <Text style={tw`text-xl text-white underline mb-10`}>
+          Matches You Have Rated
+        </Text>
+        <ScrollView
+          nestedScrollEnabled={true}
+          showsVerticalScrollIndicator={false}
+          style={tw`w-9/10 h-[140]`}
+        >
+          {isError ? (
+            <Text>There seems to be an error..</Text>
+          ) : isLoading ? (
+            <ActivityIndicator />
+          ) : (
+            userRatings.map((match, i) => (
+              <MatchRow
+                key={match.match_id}
+                match={match}
+                eventTitle={match.event_title}
+                display="Else"
+                hideBottomBorder={i === userRatings.length - 1}
+              />
+            ))
+          )}
+        </ScrollView>
+      </View>
+    </ScrollView>
   );
 }

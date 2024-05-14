7e142010349f8d7faae874ee098d6ce2148d9eb2
Brandon Gomez <bgomez9212@gmail.com>
2024-05-13 20:36:24 -0700

    changed event page to flatList

--- client/app/(tabs)/Home/[eventId].tsx
+++ client/app/(tabs)/Home/[eventId].tsx
@@ -1,5 +1,12 @@
 import { useGlobalSearchParams, useLocalSearchParams } from "expo-router";
-import { Image, SafeAreaView, ScrollView, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 import tw from "../../../tailwind";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
@@ -40,42 +47,50 @@ export default function EventPage() {
     );
   }
 
+  function eventHeader() {
+    return (
+      <View>
+        <Image
+          style={{
+            flex: 1,
+            width: undefined,
+            height: 200,
+            resizeMode: "contain",
+            marginTop: 4,
+          }}
+          source={photoLibrary[event.promotion_name]}
+        />
+        <View style={tw`mt-8 mb-8 items-center`}>
+          <Text style={tw`text-white text-3xl text-center pb-2`}>
+            {event.title}
+          </Text>
+          <Text style={tw`text-white pb-2`}>{event.date}</Text>
+          <Text style={tw`text-white pb-2`}>{event.venue_name}</Text>
+          <Text style={tw`text-white pb-2`}>
+            {event.city}, {event.state} ({event.country})
+          </Text>
+        </View>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView style={tw`flex-1`}>
-      <ScrollView style={tw`bg-darkGrey`}>
-        <View>
-          <Image
-            style={{
-              flex: 1,
-              width: undefined,
-              height: 200,
-              resizeMode: "contain",
-              marginTop: 4,
-            }}
-            source={photoLibrary[event.promotion_name]}
-          />
-          <View style={tw`mt-8 mb-8 items-center`}>
-            <Text style={tw`text-white text-3xl text-center pb-2`}>
-              {event.title}
-            </Text>
-            <Text style={tw`text-white pb-2`}>{event.date}</Text>
-            <Text style={tw`text-white pb-2`}>{event.venue_name}</Text>
-            <Text style={tw`text-white pb-2`}>
-              {event.city}, {event.state} ({event.country})
-            </Text>
-          </View>
-          <View style={tw`px-3`}>
-            {event.matches.map((match: Match, i: number) => (
-              <MatchRow
-                key={match.match_id}
-                match={match}
-                display="Event"
-                hideBottomBorder={i === event.matches.length - 1}
-              />
-            ))}
-          </View>
-        </View>
-      </ScrollView>
+      <View style={tw`bg-darkGrey`}>
+        <FlatList
+          style={tw`px-3`}
+          ListHeaderComponent={eventHeader}
+          keyExtractor={(item) => item.match_id}
+          data={event.matches}
+          renderItem={({ item, index }) => (
+            <MatchRow
+              match={item}
+              display="Event"
+              hideBottomBorder={event.matches.length - 1 === index}
+            />
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 }

34156773ae00b4496953f4e9fcd63df1b2377e97
Brandon Gomez <bgomez9212@gmail.com>
2024-05-29 22:28:00 -0700

    success message

--- client/app/ResetPasswordModal.tsx
+++ client/app/ResetPasswordModal.tsx
@@ -21,29 +21,39 @@ export default function ResetPasswordModal() {
       .then(() => setUiState({ loading: false, success: true }))
       .catch((err) => console.log(err));
   }
+
   return (
     <View style={tw`flex-1 bg-darkGrey items-center justify-center`}>
-      <TextInput
-        style={tw`w-60 bg-white h-10 p-4 mb-2 rounded p-3`}
-        textContentType="emailAddress"
-        autoCapitalize="none"
-        onChangeText={(text) => setEmail(text)}
-        placeholder="email"
-      />
-      {uiState.loading ? (
-        <View
-          style={tw`bg-blue w-60 flex items-center justify-center p-1.6 mb-2 rounded`}
-        >
-          <ActivityIndicator color="white" />
+      {uiState.success ? (
+        <View style={tw`items-center justify-center`}>
+          <Text style={tw`text-white text-2xl mb-5`}>Email Sent!</Text>
+          <LandingLink fn={() => router.back()} text={"close"} />
         </View>
       ) : (
-        <LandingButton
-          fn={resetPassword}
-          text={"Reset Password"}
-          disabled={false}
-        />
+        <View style={tw`items-center justify-center`}>
+          <TextInput
+            style={tw`w-60 bg-white h-10 p-4 mb-2 rounded p-3`}
+            textContentType="emailAddress"
+            autoCapitalize="none"
+            onChangeText={(text) => setEmail(text)}
+            placeholder="email"
+          />
+          {uiState.loading ? (
+            <View
+              style={tw`bg-blue w-60 flex items-center justify-center p-1.6 mb-2 rounded`}
+            >
+              <ActivityIndicator color="white" />
+            </View>
+          ) : (
+            <LandingButton
+              fn={resetPassword}
+              text={"Reset Password"}
+              disabled={false}
+            />
+          )}
+          <LandingLink fn={() => router.back()} text={"cancel"} />
+        </View>
       )}
-      <LandingLink fn={() => router.back()} text={"cancel"} />
     </View>
   );
 }

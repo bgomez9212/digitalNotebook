a8fac3c4e0ba827331d81ca772f9e4fc27472a46
Brandon Gomez <bgomez9212@gmail.com>
2024-06-22 22:31:16 -0700

    fix navigation from promotions page to eventId page

--- client/app/(tabs)/Profile/RatingsExtended.tsx
+++ client/app/(tabs)/Profile/RatingsExtended.tsx
@@ -1,9 +1,22 @@
 import { View, Text } from "react-native";
 import tw from "../../../tailwind";
 import { useGlobalSearchParams } from "expo-router";
+import { useQuery } from "@tanstack/react-query";
+import { getUserRatings } from "../../../api/users";
+import { getAuth } from "firebase/auth";
 
 export default function RatingsExtended() {
   const { promotionName } = useGlobalSearchParams();
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const {
+    data: userRatings,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ["userRatings"],
+    queryFn: () => getUserRatings(user.uid),
+  });
   return (
     <View style={tw`flex-1 justify-center items-center bg-darkGrey`}>
       <Text style={tw`text-white`}>More here!</Text>

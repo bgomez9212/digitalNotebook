4a035b410eeb50c689d9847079942d7d5905ba65
Brandon Gomez <bgomez9212@gmail.com>
2024-06-24 21:55:00 -0700

    flatlist added to ratings extended

--- client/app/(tabs)/Profile/RatingsExtended.tsx
+++ client/app/(tabs)/Profile/RatingsExtended.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import tw from "../../../tailwind";
 import { useGlobalSearchParams } from "expo-router";
 import { useQuery } from "@tanstack/react-query";
 import { getUserRatings } from "../../../api/users";
 import { getAuth } from "firebase/auth";
+import MatchRow from "../../../components/MatchRow";
 
 export default function RatingsExtended() {
   const { promotionName } = useGlobalSearchParams();
@@ -17,9 +18,17 @@ export default function RatingsExtended() {
     queryKey: ["userRatings"],
     queryFn: () => getUserRatings(user.uid),
   });
+  console.log(userRatings);
   return (
     <View style={tw`flex-1 justify-center items-center bg-darkGrey`}>
-      <Text style={tw`text-white`}>More here!</Text>
+      <FlatList
+        className="w-[90%]"
+        data={userRatings}
+        renderItem={({ item }) => (
+          <MatchRow match={item} display="Search" hideBottomBorder={false} />
+        )}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }

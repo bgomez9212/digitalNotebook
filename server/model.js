ae4c71b35882a4efdf810fc9b0ac6331846c05d3
Brandon Gomez <bgomez9212@gmail.com>
2024-09-09 14:51:54 -0700

    add helper function to format teams in participants

--- server/model.js
+++ server/model.js
@@ -1,10 +1,24 @@
 const pool = require("./db.js");
 
+function parseParticipantArr(arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (i === arr.length - 1) {
+      str += `${arr[i]}`;
+    } else if (i === arr.length - 2) {
+      str += `${arr[i]} & `;
+    } else {
+      str += `${arr[i]}, `;
+    }
+  }
+  return str;
+}
+
 function formatData(obj) {
   const matchObj = { ...obj };
   matchObj.championships = obj.championships.join(" & ");
   matchObj.participants = obj.participants
-    .map((participantsList, i) => participantsList.join(" & "))
+    .map((participantsList, i) => parseParticipantArr(participantsList))
     .join(" vs. ");
   return matchObj;
 }

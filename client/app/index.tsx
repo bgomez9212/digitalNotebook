a293045534b1d6c6af58373b1c5d69a887532533
Brandon Gomez <bgomez9212@gmail.com>
2024-10-10 18:57:50 -0700

    change splash screen to reflect dark and light modes

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,10 +1,20 @@
+import { useColorScheme } from "nativewind";
 import { View, Image } from "react-native";
 
 export default function index() {
-  return (
+  const { colorScheme } = useColorScheme();
+  return colorScheme === "dark" ? (
     <View className="flex-1 justify-center items-center bg-black">
       <Image
         source={require("../assets/Notebook-dark.png")}
+        resizeMode="contain"
+        className="w-full"
+      />
+    </View>
+  ) : (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Image
+        source={require("../assets/Notebook-light.png")}
         resizeMode="contain"
         className="w-full"
       />

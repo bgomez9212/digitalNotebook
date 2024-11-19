2247a8254646521b79ce37574ddefd71b7877395
Brandon Gomez <bgomez9212@gmail.com>
2024-11-18 17:09:56 -0800

    add a limiter to server to test slow load times

--- server/index.js
+++ server/index.js
@@ -3,7 +3,14 @@ const app = express();
 const cors = require("cors");
 const router = require("./router.js");
 const dotenv = require("dotenv").config();
+const { slowDown } = require("express-slow-down");
 
+const limiter = slowDown({
+  delayAfter: 0,
+  delayMs: () => 2000,
+});
+
+// app.use(limiter);
 app.use(cors());
 app.use(express.json());
 

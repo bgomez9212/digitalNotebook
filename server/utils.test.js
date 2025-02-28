51b399ea554fedff63e147ce4e42ee31f4f1c8d9
Brandon Gomez <bgomez9212@gmail.com>
2025-02-28 09:41:21 -0800

    format data test

--- server/utils.test.js
+++ server/utils.test.js
@@ -11,3 +11,37 @@ const {
 test("parseParticipantArr", () => {
   expect(parseParticipantArr(["A", "B", "C"])).toBe("A, B & C");
 });
+
+test("formatData", () => {
+  expect(
+    formatData({
+      match_id: 671,
+      event_id: 131,
+      event_title: "Battle Of The Belts XII",
+      promotion: "AEW",
+      participants: [
+        ["Claudio Castagnoli", "PAC", "Wheeler Yuta"],
+        ["Alex Reynolds", "Evil Uno", "John Silver"],
+      ],
+      championships: ["AEW World Trios Title"],
+      date: undefined,
+      user_rating: null,
+      community_rating: "2.00",
+      rating_count: 1,
+      rating_date: undefined,
+    })
+  ).toStrictEqual({
+    match_id: 671,
+    event_id: 131,
+    event_title: "Battle Of The Belts XII",
+    promotion: "AEW",
+    participants:
+      "Claudio Castagnoli, PAC & Wheeler Yuta vs. Alex Reynolds, Evil Uno & John Silver",
+    championships: "AEW World Trios Title",
+    date: undefined,
+    user_rating: null,
+    community_rating: "2.00",
+    rating_count: 1,
+    rating_date: undefined,
+  });
+});

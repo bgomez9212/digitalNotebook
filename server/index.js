d1d0b9623e3dd1a6309887a214fb6ea85d3be26c
Brandon Gomez <bgomez9212@gmail.com>
2025-03-26 17:34:46 -0700

    move api to its own folder and create server

--- server/index.js
+++ server/index.js
@@ -1,24 +1,26 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
 const cors = require("cors");
-const router = require("./router.js");
 const dotenv = require("dotenv").config();
-const { slowDown } = require("express-slow-down");
-const apiKeyMiddleware = require("./apiKeyMiddleware");
+const port = 3001;
 
-const limiter = slowDown({
-  delayAfter: 0,
-  delayMs: () => 2000,
-});
-
-// app.use(limiter);
 app.use(cors());
 app.use(express.json());
-app.use(apiKeyMiddleware);
-app.use("/api", router);
 
-app.listen(process.env.PORT, "0.0.0.0", () => {
-  console.log(`Server running`);
+app.get("/*", (req, res) => {
+  try {
+    axios
+      .get(`http://localhost:3000/api${req._parsedUrl.pathname}`, {
+        params: req.query,
+      })
+      .then((response) => res.send(response.data))
+      .catch((err) => console.error(err));
+  } catch (err) {
+    console.log("an error has occured");
+  }
 });
 
-module.exports = app;
+app.listen(port, () => {
+  console.log("server listening");
+});

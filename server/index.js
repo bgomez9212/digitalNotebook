f35dfc51a417838426d67057ada79710f5a039bf
Brandon Gomez <bgomez9212@gmail.com>
2025-03-26 18:53:35 -0700

    catchall for server with axios

--- server/index.js
+++ server/index.js
@@ -8,12 +8,13 @@ const port = 3001;
 app.use(cors());
 app.use(express.json());
 
-app.get("/*", (req, res) => {
+app.use("/*", (req, res) => {
   try {
-    axios
-      .get(`http://localhost:3000/api${req._parsedUrl.pathname}`, {
-        params: req.query,
-      })
+    axios({
+      method: req.method,
+      url: `http://localhost:3000/api${req._parsedUrl.pathname}`,
+      params: req.query,
+    })
       .then((response) => res.send(response.data))
       .catch((err) => console.error(err));
   } catch (err) {

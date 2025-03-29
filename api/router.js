9d780368fe7b2ce4657cf3662c6baab40edea839
Brandon Gomez <bgomez9212@gmail.com>
2025-03-28 17:53:08 -0700

    change some routes to match RESTful practices

--- api/router.js
+++ api/router.js
@@ -2,34 +2,27 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./controller.js");
 
-// number - optional query param (number of results)
 router.get("/events/recent", controller.getRecentEvents);
-// user_id - optional query param (include specific user's ratings)
 router.get("/events/:event_id", controller.getEvent);
 router.post("/events", controller.postEvent);
 
-// number - optional query param (number of matches)
 router.get("/matches/topRated", controller.getTopRatedMatches);
+router.post("/matches/:match_id/ratings", controller.postRating);
+router.delete("/matches/:match_id/ratings", controller.deleteUserRating);
 
-router.get("/ratings/:user_id", controller.getUsersRatedMatches);
-// body includes user_id and rating
-router.post("/ratings/:match_id", controller.postRating);
-// body includes user_id
-router.delete("/ratings/:match_id", controller.deleteUserRating);
-// query params - search_param, query, user_id
 router.get("/search", controller.getSearchResults);
 
 router.get("/promotions", controller.getPromotions);
 
+router.get("/users/:user_id/ratings", controller.getUsersRatedMatches);
+
 router.get("/users/:user_id", controller.getUsername);
-// body - username
-router.put("/users/:user_id", controller.editUsername);
-// username - query param
-router.get("/users", controller.checkUsernames);
-//
 router.post("/users", controller.createUser);
+router.put("/users/:user_id", controller.editUsername);
+
+router.get("/users", controller.checkUsernames);
 router.delete("/users", controller.deleteUser);
-// user_id - query param
+
 router.get("/wrestlers/:wrestler_id", controller.getWrestlerMatches);
 
 router.get("/info", controller.getInfo);

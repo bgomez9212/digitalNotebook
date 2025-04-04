402910329a16a22b42eccfa5140b8c63d87f9f90
Brandon Gomez <bgomez9212@gmail.com>
2025-04-03 17:32:11 -0700

    temp fix plus text color for dark

--- client/components/LandingButton.tsx
+++ client/components/LandingButton.tsx
@@ -20,14 +20,14 @@ export default function LandingButton({
 }: LandingButtonProps) {
   return loading ? (
     <View
-      className={`bg-${color ? color : "blue"} w-${width ? width : 60} flex items-center justify-center h-9 my-2 rounded`}
+      className={`bg-blue w-${width ? width : 60} flex items-center justify-center h-9 my-2 rounded`}
     >
       <ActivityIndicator color="white" />
     </View>
   ) : (
     <TouchableOpacity
       disabled={disabled}
-      className={`bg-${color ? color : "blue"} w-${width ? width : 60} flex items-center justify-center rounded ${disabled ? "opacity-50" : ""} my-2 h-9`}
+      className={`bg-blue w-${width ? width : 60} flex items-center justify-center rounded ${disabled ? "opacity-50" : ""} my-2 h-9`}
       onPress={fn}
     >
       <Text className="text-white">{text}</Text>

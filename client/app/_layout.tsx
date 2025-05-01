8931f6303cc410022d509b657d6481c5a1fc843b
Brandon Gomez <bgomez9212@gmail.com>
2025-04-30 21:17:35 -0700

    add new library and use in entry point

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -4,6 +4,7 @@ import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
 import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { KeyboardProvider } from "react-native-keyboard-controller";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 const queryClient = new QueryClient();
 const blue = "#618BCB";
@@ -19,53 +20,55 @@ export default function Layout() {
     });
   });
   return (
-    <QueryClientProvider client={queryClient}>
-      <GestureHandlerRootView>
-        <BottomSheetModalProvider>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen
-              name="index"
-              options={{
-                headerShown: false,
-                animation: "fade",
-                animationDuration: 1000,
-              }}
-            />
-            <Stack.Screen
-              name="Landing"
-              options={{
-                headerShown: false,
-                animation: "fade",
-                animationDuration: 1000,
-              }}
-            />
-            <Stack.Screen
-              name="RatingModal"
-              options={{
-                presentation: "modal",
-                title: "Submit a Rating",
-                headerStyle: { backgroundColor: blue },
-                headerTitleStyle: { color: "white" },
-                headerShown: true,
-              }}
-            />
-            <Stack.Screen
-              name="(tabs)"
-              options={{ animation: "fade", animationDuration: 1000 }}
-            />
-            <Stack.Screen
-              name="ResetPasswordModal"
-              options={{
-                presentation: "modal",
-                title: "Reset Password",
-                headerStyle: { backgroundColor: blue },
-                headerTitleStyle: { color: "white" },
-                headerShown: true,
-              }}
-            />
-          </Stack>
-        </BottomSheetModalProvider>
-      </GestureHandlerRootView>
-    </QueryClientProvider>
+    <KeyboardProvider>
+      <QueryClientProvider client={queryClient}>
+        <GestureHandlerRootView>
+          <BottomSheetModalProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen
+                name="index"
+                options={{
+                  headerShown: false,
+                  animation: "fade",
+                  animationDuration: 1000,
+                }}
+              />
+              <Stack.Screen
+                name="Landing"
+                options={{
+                  headerShown: false,
+                  animation: "fade",
+                  animationDuration: 1000,
+                }}
+              />
+              <Stack.Screen
+                name="RatingModal"
+                options={{
+                  presentation: "modal",
+                  title: "Submit a Rating",
+                  headerStyle: { backgroundColor: blue },
+                  headerTitleStyle: { color: "white" },
+                  headerShown: true,
+                }}
+              />
+              <Stack.Screen
+                name="(tabs)"
+                options={{ animation: "fade", animationDuration: 1000 }}
+              />
+              <Stack.Screen
+                name="ResetPasswordModal"
+                options={{
+                  presentation: "modal",
+                  title: "Reset Password",
+                  headerStyle: { backgroundColor: blue },
+                  headerTitleStyle: { color: "white" },
+                  headerShown: true,
+                }}
+              />
+            </Stack>
+          </BottomSheetModalProvider>
+        </GestureHandlerRootView>
+      </QueryClientProvider>
+    </KeyboardProvider>
   );
 }
